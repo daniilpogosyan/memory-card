b47b0c89815b545158fa28363fea5112d20389ad
Daniil Pogosyan <daniilpogosyan@gmail.com>
2022-07-16 15:55:12 +0500

    Add style to Cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,15 @@
+import './styles/Card.css'
+
 export default function Card(props) {
   return (
-    <article onClick={(e) => props.handleClick(e, props.card.name)}>
-      <img src={props.card.imgUrl} alt={props.card.name} />
-      <span>{props.card.name}</span>
+    <article
+      className="card"
+      onClick={(e) => props.handleClick(e, props.card.name)}>
+      <img 
+        className="card__img"
+        src={props.card.imgUrl}
+        alt={props.card.name} />
+      <span className="card__title">{props.card.name}</span>
     </article>
   )
 }
